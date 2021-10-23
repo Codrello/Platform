9e831df5cc94a00130b7b191b5eebe1c02afa29c
Codrello <Coder7823@mail.ru>
2021-10-24 01:52:00 +0500

    token va user va vidInf qoshildi

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,43 @@ const Login = require("../helper/Middleware")
 const Allvid = require("../helper/AllvidMiddleware")
 const Passport = require("../passport/passport")
 const User = require("../model/Users")
+const Video = require("../model/Video")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 /* GET home page. */
-router.get('/list', Login, function(req, res, next) {
-  
+router.get('/list', Login, function (req, res, next) {
+
   const user = req.user;
   // const token = req.token
   const decod = req.decoded.user._id
   // console.log(decod)
-  User.findById(decod,(err, dec) => {
-    if(err){
-  console.log(err)
+  User.findById(decod, (err, dec) => {
+    if (err) {
+      console.log(err)
 
     }
-    res.json({dec});
+    res.json({ dec });
 
   })
   // res.render("list", {user})
 });
-router.get('/Allvid',Allvid, function(req, res, next) {
-  
+router.get('/vidInf', function (req, res, next) {
+
+  const user = req.user;
+  Video.find({}, (err, videos) => {
+    if (err) {
+      console.log(err)
+
+    }
+    res.json({ videos });
+
+  })
+});
+router.get('/Allvid', Allvid, function (req, res, next) {
+
   const user = req.user;
 
   res.send("Hamma videolar");
